1cb604a1b157ae446344fd696aaad8a12dd94fd3
Lucas443-web <johnreliable145@gmail.com>
2025-07-12 12:26:29 -0300

    etapa-5 - Etapas 5 e 6

---
+++ conta_bancaria/src/repository/ContaRepository.ts
@@ -0,0 +1,20 @@
+import { Conta } from "/Users/user/Documents/conta-bancaria-fix/conta_bancaria/conta_bancaria/src/model/Conta";
+
+export interface ContaRepository {
+
+	// CRUD da Conta
+	procurarPorNumero(numero: number): void;
+	listarTodas(): void;
+	cadastrar(conta: Conta): void;
+	atualizar(conta: Conta): void;
+	deletar(numero: number): void;
+	
+	// Métodos Bancários
+	sacar(numero: number, valor: number): void;
+	depositar(numero: number, valor: number): void;
+	transferir(numeroOrigem: number, numeroDestino: number, valor: number): void;
+	
+}
+
+
+
